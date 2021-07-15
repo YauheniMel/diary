b90a70c99a0b1b3ffebf6998c518d01493f5c31f
EkaterinaLapunova <katyalapunova96@gmail.com>
2021-07-15 22:05:56 +0300

    Add form-values.js

--- src/app.js
+++ src/app.js
@@ -1,1 +1,6 @@
 import './style.scss';
+import FormValues from './js/form-memory/form-values.js';
+
+const formMemoryEl = document.querySelector('.form-memory');
+
+new FormValues(formMemoryEl);
---
+++ src/js/form-memory/form-values.js
@@ -0,0 +1,13 @@
+class FormValues {
+  constructor(form) {
+    this.form = form;
+
+    this.init();
+  }
+
+  init() {
+    console.log(this.form);
+  }
+}
+
+export default FormValues;
