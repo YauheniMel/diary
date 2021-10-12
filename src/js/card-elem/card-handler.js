78f4ad370c4f61590bbb44bb7edb68e05c9f9326
YauheniMel <207melnik@gmail.com>
2021-10-12 10:43:26 +0300

    feat: create calendar

--- src/js/card-elem/card-handler.js
+++ src/js/card-elem/card-handler.js
@@ -20,54 +20,67 @@ class CardHandler {
   }
 
   renderCard(data) {
-    this.getSortArr(data);
-
+    const copyData = data.slice();
+    const calendar = this.buildCalendar(data);
     this.sectionMemoryEl.innerHTML = '';
-    const template = `
-      <h2 class="memory__date">июль 2021</h2>
-      <div class="memory__wrap-card"></div>
-    `;
-
-    const articleEl = document.createElement('article');
-    articleEl.innerHTML = template;
-    const wrapCardEl = articleEl.querySelector('.memory__wrap-card');
 
     let arrLocalStorage = JSON.parse(localStorage.getItem('collage'));
 
-    data.forEach((item, index) => {
-      const templateCardEl = templateCard.content.cloneNode(true);
-      const cardEl = templateCardEl.querySelector('.card');
-      const cardTitleEl = templateCardEl.querySelector('.card__title');
-      const cardDateEl = templateCardEl.querySelector('.card__date');
-      const btnDelCardEl = templateCardEl.querySelector('.card__btn-delete');
-      const cardImageEl = templateCardEl.querySelector('.card__image');
+    calendar.forEach((item) => {
+      copyData.forEach((item2) => {
+        if(item2.id == item.id) {
 
-      for(let key in arrLocalStorage) {
-        if(arrLocalStorage[key].id == item.id) {
-          cardImageEl.style.cssText = `
-            opacity: 1;
+          const templateCardEl = templateCard.content.cloneNode(true);
+          const cardEl = templateCardEl.querySelector('.card');
+          const cardTitleEl = templateCardEl.querySelector('.card__title');
+          const cardDateEl = templateCardEl.querySelector('.card__date');
+          const btnDelCardEl = templateCardEl.querySelector('.card__btn-delete');
+          const cardImageEl = templateCardEl.querySelector('.card__image');
+
+          cardEl.setAttribute('id', item.id);
+
+          cardEl.addEventListener('click', () => this.handleClickCard());
+          btnDelCardEl.addEventListener('click', () => this.handleClickDeleteCard());
+
+          cardEl.style.cssText = `
+            background: url(./photo/${item2.imageName}) no-repeat;
+            background-size: 100% auto;
+            overflow: hidden;
+            background-position-y: center;
           `;
+
+          cardTitleEl.innerHTML = item2.title;
+          cardDateEl.innerHTML = item2.date;
+
+          if(document.querySelector(`[data-date="${item.date}"]`)){
+            const wrapper = document.querySelector(`[data-date="${item.date}"]`);
+
+            wrapper.append(cardEl);
+          } else {
+            const template = `
+              <h2 class="memory__date">${item.date}</h2>
+              <div class="memory__wrap-card"></div>
+            `;
+
+            const articleEl = document.createElement('article');
+            articleEl.innerHTML = template;
+            const wrapCardEl = articleEl.querySelector('.memory__wrap-card');
+            wrapCardEl.setAttribute('data-date', item.date);
+
+            wrapCardEl.append(cardEl);
+
+            this.sectionMemoryEl.append(articleEl);
+          }
+
+          for(let key in arrLocalStorage) {
+            if(arrLocalStorage[key].id == item.id) {
+              cardImageEl.style.cssText = `
+                opacity: 1;
+              `;
+            }
+          }
         }
-      }
-
-      cardEl.setAttribute('id', item.id);
-
-      cardEl.addEventListener('click', () => this.handleClickCard());
-      btnDelCardEl.addEventListener('click', () => this.handleClickDeleteCard());
-
-      cardEl.style.cssText = `
-        background: url(./photo/${item.imageName}) no-repeat;
-        background-size: 100% auto;
-        overflow: hidden;
-        background-position-y: center;
-      `;
-
-      cardTitleEl.innerHTML = item.title;
-      cardDateEl.innerHTML = item.date;
-
-      wrapCardEl.append(cardEl);
-
-      this.sectionMemoryEl.append(articleEl);
+      })
     });
   }
 
@@ -166,7 +179,90 @@ class CardHandler {
   }
 
   getSortArr(arr) {
-    console.log(arr);
+    const arrNum = [];
+
+    arr.forEach(item => {
+      const num = item.date.match(/\d/gi).join("");
+
+      arrNum.push(num);
+    })
+
+    arrNum.sort();
+
+    let sortArr = [];
+
+    arrNum.forEach(item => {
+      arr.forEach((item2, index) => {
+        const num = item2.date.match(/\d/g).join("");
+        if(item == num) {
+          sortArr.push(item2);
+          arr.splice(index, 1);
+        }
+      })
+    })
+
+    return sortArr;
+  }
+
+  buildCalendar(data) {
+    const sortArr = this.getSortArr(data);
+
+    const dateArr = [];
+    sortArr.forEach(item => {
+
+      const year = item.date.match(/[0-9]+/);
+      const str = item.date.match(/-[0-9]+-/);
+      const month = str[0].match(/[0-9]+/);
+      const nameMonth = this.getNameMonth(month[0]);
+
+      const date = nameMonth + ' ' + year[0];
+      let n = 0;
+      let obj = {};
+      // if(dateArr[0]) {
+
+      //   for(let key in dateArr) {
+      //     if(dateArr[key].date == date) return n++;
+      //   }
+      //   if(n == 0) {
+      //     obj.date = date;
+      //     obj.id = item.id;
+      //     dateArr.push(obj);
+      //   }
+      // } else {
+        obj.date = date;
+        obj.id = item.id;
+
+        dateArr.push(obj);
+      // }
+    })
+
+    return dateArr;
+  }
+
+  getNameMonth(num) {
+    if(num == 1) {
+      return 'январь';
+    } else if(num == 2) {
+      return 'февраль';
+    } else if(num == 3) {
+      return 'март';
+    } else if(num == 4) {
+      return 'апрель';
+    } else if(num == 5) {
+      return 'май';
+    } else if(num == 6) {
+      return 'июнь';
+    } else if(num == 7) {
+      return 'июль';
+    } else if(num == 8) {
+      return 'август';
+    } else if(num == 9) {
+      return 'сентябрь';
+    } else if(num == 10) {
+      return 'октябрь';
+    } else if(num == 11) {
+      return 'ноябрь';
+    } else return 'декабрь';
   }
 }
 
